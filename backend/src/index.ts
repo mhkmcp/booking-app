1a916ad70dbf2806dde22a0706b1b5e88617cd53
Humayun Kabir <mhkmcp@yahoo.com>
2024-04-03 23:57:25 +0600

    added user register api

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,8 @@ import cors from 'cors'
 import "dotenv/config"
 import mongose from "mongoose"
 
+import userRoutes from "./routes/users"
+
 mongose.connect(process.env.MONGODB_CONNECTION_STRING as string)
 
 const app = express()
@@ -10,9 +12,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cors())
 
-app.get("/api/test", async (req: Request, res: Response) => {
-    res.json({'message': "Hello Express!"})
-})
+app.use("/api/v1/users", userRoutes)
 
 app.listen(7000, () => {
     console.log("Server running on localhost:7000")
